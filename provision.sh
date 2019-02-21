9270d2e6d46a2c4e0ace7e936e041e34797ae15f
Aldi Alimucaj <aldi.alimucaj@gmail.com>
2019-02-21 13:57:22 +0100

    [FEATURE] openssl to generate hmac for provision.sh

--- provision.sh
+++ provision.sh
@@ -9,6 +9,16 @@ PASSWORD_TEMPLATE="#password#"
 ADMIN_PASSWORD_TEMPLATE="#admin_pwd#"
 JWT_TEMPLATE="#jwt_secret#"
 
+set -e
+
+function verifyProgram {
+    if [ $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed") -eq 0 ];
+    then
+        echo "$1 is required but not found. Attempting to installed if it fails install manually to proceede."
+        echo "sudo apt-get install $1;"
+        sudo apt-get install $1;
+    fi
+}
 
 # backup config files
 function backup {
@@ -56,7 +66,9 @@ function provision {
     # generated jwt secret
     GENERATED_JWT_SECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9!?:;_=' | fold -w $PASSWORD_LENGTH | head -n 1)
     # generate admin pwd based on jwt secret
-    ADMIN_INITIAL_PWD=$(node -e "console.log(require('./server/src/helpers/utils.js').sha512('admin','$GENERATED_JWT_SECRET'))")
+    verifyProgram "openssl"
+    ADMIN_INITIAL_PWD=$(echo -n "admin" | openssl sha512 -hmac "$GENERATED_JWT_SECRET" | sed "s/(stdin)= //")
+    
     echo ""
     echo -e "Database password: \t\e[31m$GENERATED_PWD\e[0m"
     echo -e "JWT Secret: \t\t\e[31m$GENERATED_JWT_SECRET\e[0m"
